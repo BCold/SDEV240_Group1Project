26b2b24fac35c0bee38a7f14e5bcebc3d0733192
Brandon Coldren <BCold93@protonmail.com>
2023-03-08 17:55:15 -0500

    Group 1 project initial git commit

---
+++ group_1_final/group_1_Final/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace group_1_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2615, 1080);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Materials List Estimator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 941);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2615, 139);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Group 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 828);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(2615, 113);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(48, 48);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(2615, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2615, 1080);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Materials List Estimator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ group_1_final/group_1_Final/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace group_1_Final
+{
+    public partial class Form2 : System.Windows.Forms.Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add("Flooring", "Concrete","Ready to Mix Concrete", "50 lb bag",null, 10.99);
+            dataGridView1.Rows.Add("Flooring", "Rebar", "Steel", "1/2 inch x 10 ft", null, 4.99);
+            dataGridView1.Rows.Add("Flooring", "Tile", "Flat Clay Flooring","3 inches by 12 inches", null, 3.99);
+            dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
+            dataGridView1.Rows.Add(null, null, null, null, null, null, null);
+            dataGridView1.Rows.Add("Walls", "Dry wall", "Walls", "1/2-in x 4-ft x 8-ft", null, 11.99);
+            dataGridView1.Rows.Add("Walls", "Interior Paint", "Low Odor Paint Semi Gloss", "1 Quart", null, 14.99);
+            dataGridView1.Rows.Add("Walls", "Exterior Paint", "Latex Paint", "5 gallon", null, 35.99);
+            dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
+            dataGridView1.Rows.Add(null, null, null, null, null, null, null);
+            dataGridView1.Rows.Add("Windows", "Single Hung Window", "Vinyl insulated with grids", "35.5-in", null, 227.99);
+            dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
+            dataGridView1.Rows.Add(null, null, null, null, null, null, null);
+            dataGridView1.Rows.Add("Door", "Modern Fiberglass Door", "Fiberglass for durability", "36-in x 80-in", null, 959.99);
+            dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
+            dataGridView1.Rows.Add(null, null, null, null, null, "Grand total", null);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }//end of dgv1 
+
+        
+    }
+}
