dbe806297c254e1b022cae46fa466436f746dd9b
Brandon Coldren <BCold93@protonmail.com>
2023-03-10 17:34:48 -0500

    CalcMaterialTotal Method created CalcGrantTotal Method created DataGridView1_CellMouseUp EventHandler created ContextMenuStrip1_Click EventHandler created DataGridView1_CellLeave EventHandler created DataGridView1_CellEnter EventHandler created

--- group_1_final/group_1_Final/Form2.Designer.cs
+++ group_1_final/group_1_Final/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.categoryColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.itemColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -36,7 +37,11 @@
             this.quantityColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.unitCostColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.costColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -52,16 +57,18 @@
             this.quantityColumn,
             this.unitCostColumn,
             this.costColumn});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 123;
             this.dataGridView1.RowTemplate.Height = 46;
-            this.dataGridView1.Size = new System.Drawing.Size(2449, 1290);
+            this.dataGridView1.Size = new System.Drawing.Size(2449, 1214);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellEnter);
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellLeave);
+            this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseUp);
             // 
             // categoryColumn
             // 
@@ -112,11 +119,35 @@
             this.costColumn.MinimumWidth = 15;
             this.costColumn.Name = "costColumn";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(134, 26);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.ContextMenuStrip1_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(133, 22);
+            this.toolStripMenuItem1.Text = "Delete Row";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1242, 1235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "tempLabel";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2449, 1290);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -125,7 +156,9 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +171,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn quantityColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn unitCostColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn costColumn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- group_1_final/group_1_Final/Form2.cs
+++ group_1_final/group_1_Final/Form2.cs
@@ -12,22 +12,50 @@ namespace group_1_Final
 {
     public partial class Form2 : System.Windows.Forms.Form
     {
+        private int rowIndex = 0;
         public Form2()
         {
             InitializeComponent();
-            
+
+        }
+        void CalcMaterialTotal()
+        // Calculates the total cost of a material based on input quantity and unit pricing
+        {
+            for (int i = 0; i < dataGridView1.RowCount; ++i)
+            {
+                if (dataGridView1.Rows[i].Cells[4].Value != null && dataGridView1.Rows[i].Cells[5].Value != null)
+                {
+                    int quantity = Convert.ToInt32(dataGridView1.Rows[i].Cells[4].Value);
+                    double unitPrice = Convert.ToDouble(dataGridView1.Rows[i].Cells[5].Value);
+                    dataGridView1.Rows[i].Cells[6].Value = (quantity * unitPrice).ToString("F2");
+                }
+            }
+        }
+
+        void CalcGrandTotal()
+        {
+            double grandTotal = 0;
+            for (int i = 0; i < dataGridView1.RowCount; ++i)
+            {
+                if (dataGridView1.Rows[i].Cells[6].Value != null)
+                {
+                    grandTotal += Convert.ToDouble(dataGridView1.Rows[i].Cells[6].Value);
+                }
+            }
+            label1.Text = grandTotal.ToString("F2");
+
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            dataGridView1.Rows.Add("Flooring", "Concrete","Ready to Mix Concrete", "50 lb bag",null, 10.99);
-            dataGridView1.Rows.Add("Flooring", "Rebar", "Steel", "1/2 inch x 10 ft", null, 4.99);
-            dataGridView1.Rows.Add("Flooring", "Tile", "Flat Clay Flooring","3 inches by 12 inches", null, 3.99);
+            dataGridView1.Rows.Add("Flooring", "Concrete", "Ready to Mix Concrete", "50 lb bag", 5, 10.99);
+            dataGridView1.Rows.Add("Flooring", "Rebar", "Steel", "1/2 inch x 10 ft", 10, 4.99);
+            dataGridView1.Rows.Add("Flooring", "Tile", "Flat Clay Flooring", "3 inches by 12 inches", 20, 3.99);
             dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
             dataGridView1.Rows.Add(null, null, null, null, null, null, null);
-            dataGridView1.Rows.Add("Walls", "Dry wall", "Walls", "1/2-in x 4-ft x 8-ft", null, 11.99);
-            dataGridView1.Rows.Add("Walls", "Interior Paint", "Low Odor Paint Semi Gloss", "1 Quart", null, 14.99);
-            dataGridView1.Rows.Add("Walls", "Exterior Paint", "Latex Paint", "5 gallon", null, 35.99);
+            dataGridView1.Rows.Add("Walls", "Dry wall", "Walls", "1/2-in x 4-ft x 8-ft", 5, 11.99);
+            dataGridView1.Rows.Add("Walls", "Interior Paint", "Low Odor Paint Semi Gloss", "1 Quart", 2, 14.99);
+            dataGridView1.Rows.Add("Walls", "Exterior Paint", "Latex Paint", "5 gallon", 1, 35.99);
             dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
             dataGridView1.Rows.Add(null, null, null, null, null, null, null);
             dataGridView1.Rows.Add("Windows", "Single Hung Window", "Vinyl insulated with grids", "35.5-in", null, 227.99);
@@ -36,18 +64,41 @@ namespace group_1_Final
             dataGridView1.Rows.Add("Door", "Modern Fiberglass Door", "Fiberglass for durability", "36-in x 80-in", null, 959.99);
             dataGridView1.Rows.Add(null, null, null, null, null, "Total", null);
             dataGridView1.Rows.Add(null, null, null, null, null, "Grand total", null);
+        }
 
+        private void DataGridView1_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.dataGridView1.Rows[e.RowIndex].Selected = true;
+                this.rowIndex = e.RowIndex;
+                this.dataGridView1.CurrentCell = this.dataGridView1.Rows[e.RowIndex].Cells[1];
+                this.contextMenuStrip1.Show(this.dataGridView1, e.Location);
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void ContextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            if (!this.dataGridView1.Rows[this.rowIndex].IsNewRow)
+                this.dataGridView1.Rows.RemoveAt(this.rowIndex);
+        }
+
+        private void DataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            CalcMaterialTotal();
+            CalcGrandTotal();
+        }
+
+        private void DataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            CalcMaterialTotal();
+            CalcGrandTotal();
         }
 
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
         }
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }//end of dgv1 
-
-        
     }
 }
